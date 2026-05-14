ef289a3689b8e7c745d89cdfbab8555cb39c424a
rohitraghav95 <148754640+rohitraghav95@users.noreply.github.com>
2026-05-14 17:20:37 +0530

    Refactor addition and subtraction functions

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-def addition(a, b):
-    return a + b 
+def addition(a, b, c):
+    return a + b + c
 
-def subtraction(a, b, c):
-    return a - b - c
+def subtraction(a, b):
+    return a - b 
 
 def division(a, b)
     if b != 0:
